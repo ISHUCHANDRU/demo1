b78b09a13f274c0b6d5d3fb63665be42a481e5b4
ISHUCHANDRU <ishuchandru1996@gmail.com>
2017-05-20 10:08:50 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+Class Number
+{
+public static void main(String args[])
+{
+int x;
+System.out.println("enter an number");
+Scanner in=new Scanner(System.in);
+x=in.nextInt();
+if(x%2==0)
+System.out.println("number is even");
+else
+System.out.println("number is odd");
+}
+}
